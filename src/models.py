a1f32479012c122102616e46dd581dac6e615bd1
felixbrunner <rubelrennfix@gmail.com>
2020-09-05 20:23:22 +0100

    edit model class

--- src/models.py
+++ src/models.py
@@ -1,39 +1,77 @@
-import pandas as pd
 import numpy as np
 import warnings
+from scipy.stats import norm
 
 
-class NormalModel:
+class BaseModel:
+    
+    '''
+    Base class for models
+    '''
+    
+    def __init__(self):
+        pass
+    
+    def score(self, Y):
+        
+        '''
+        Returns the log-likelihood of an observation sequence
+        '''
+        
+        score = np.log(self.pdf(Y)).sum()
+        return score
+
+
+class NormalModel(BaseModel):
+    
+    '''
+    i.i.d. normal distribution model
+    '''
+    
     def __init__(self, mu=0, sigma=1):
         self.mu = mu
         self.sigma = sigma
+    
+    @property
+    def loc(self):
+        return self.mu
+    
+    @property
+    def scale(self):
+        return self.sigma
+    
         
     def fit(self, Y, weights=None):
-        '''fits the model parameters to an observation sequence, weights are optional'''
+        
+        '''
+        Fits the model parameters to an observation sequence.
+        weights are optional.
+        '''
+        
         # prepare
-        Y = np.array(Y).reshape(-1, 1)
+        Y = np.array(Y)
         if weights is None:
             weights = np.ones(Y.shape)
         else:
-            weights = np.array(weights).reshape(-1, 1)
+            weights = np.array(weights)
         
         # estimate mean
-        mean = (Y*weights).sum(axis=0)/weights.sum(axis=0)
+        mean = np.average(Y, weights=weights)
         
         # estimate variance
         errors = (Y-mean)**2
-        variance = (errors*weights).sum(axis=0)/weights.sum(axis=0)
+        variance = np.average(errors, weights=weights)
         
         # update
         self.mu = mean
         self.sigma = np.sqrt(variance)
         
     def pdf(self, Y):
-        '''returns the likelihood of each observation in an observation sequence'''
-        pdf = 1/(self.sigma * np.sqrt(2*np.pi)) * np.exp(-0.5*(Y-self.mu)**2/self.sigma**2)
+        
+        '''
+        Returns the likelihood of each observation in an observation sequence.
+        '''
+        
+        Y = np.array(Y)
+        pdf = norm(loc=self.mu, scale=self.sigma).pdf(Y)
         return pdf
-    
-    def score(self, Y):
-        '''returns the log-likelihood of an observation sequence'''
-        score = np.log(self.pdf(Y)).sum()
-        return score
